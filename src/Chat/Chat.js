5052f55a6b25086e63db91b522a4a0f08e7d1dc6
unknown <dev.works.naman@gmail.com>
2020-10-23 01:05:11 +0530

    final commit

---
+++ src/Chat/Chat.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react';
+import './Chat.css';
+import { useParams } from 'react-router-dom';
+import Message from './Message/Message';
+import db from '../firebase';
+import { InfoOutlined, StarBorderOutlined } from '@material-ui/icons';
+import ChatInput from './ChatInput/ChatInput';
+
+function Chat() {
+    const { roomId } = useParams();
+    const [roomDetails, setRoomDetails] = useState(null);
+    const [roomMessages, setRoomMessages] = useState(null);
+
+    useEffect(() => {
+        if(roomId) {
+            db.collection('rooms')
+            .doc(roomId)
+            .onSnapshot(snapshot => {
+                setRoomDetails(snapshot.data());
+            })
+        }
+
+        db.collection('rooms')
+        .doc(roomId)
+        .collection('messages')
+        .orderBy('timestamp', 'asc')
+        .onSnapshot(snapshot => {
+            setRoomMessages(snapshot.docs.map(doc => doc.data()))
+        })
+
+
+    }, [roomId])
+
+    console.log(roomMessages);
+
+    return (
+        <div className='chat'>
+            <div className="chat__header">
+                <div className="chat__headerLeft">
+                    <h4 className="chat__channelName">
+                        <strong> #{roomDetails?.name} </strong>
+                        <StarBorderOutlined />
+                    </h4>
+                </div>
+
+                <div className="chat__headerRight">
+                    <p><InfoOutlined /> Details</p>
+                </div>
+            </div>
+
+            <div className="chat__messages">
+                {roomMessages?.map(({ message, timestamp, user, userImage }) => (
+                    <Message
+                        message={message}
+                        timestamp={timestamp}
+                        user={user}
+                        userImage={userImage}
+                    />
+                ))}
+            </div>
+
+            <ChatInput channelName={roomDetails?.name} channelId={roomId} />
+        </div>
+    )
+}
+
+export default Chat
